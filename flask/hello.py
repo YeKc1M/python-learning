233ae26855fc8bad9a1e15dc93bb99ce22526ed8
YeKc1M <15008181891@163.com>
2020-03-23 16:20:03 +0800

    add render_template

--- flask/hello.py
+++ flask/hello.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.helpers import url_for
 app=Flask(__name__)
 
 @app.route('/')
@@ -10,5 +11,20 @@ def index():
         <h1>this is index</h1>
     </body>'''
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html',name=name)
+
+# url_for(func_name, args)
+@app.route('/try_url/')
+def try_url():
+    return '''url_for'''
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
+    # test url_for
+    # with app.test_request_context():
+    #     print(url_for('try_url'))
+    #     print(url_for('try_url',arg1='arg1',arg2='arg2'))
+
