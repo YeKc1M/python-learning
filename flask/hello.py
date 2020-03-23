c71b62be48b52d71bffc22d5d02508b17c7747ee
YeKc1M <15008181891@163.com>
2020-03-23 18:03:13 +0800

    add login(get & post)

--- flask/hello.py
+++ flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask.helpers import url_for
 app=Flask(__name__)
 
@@ -15,6 +15,25 @@ def index():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html',name=name)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error=None
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        if(username=='mky' and password=='123'):
+            return render_template('hello.html', name=username)
+        else:
+            error='Invalid username and password POST'
+    elif request.method=='GET':
+        username=request.args.get('username')
+        password=request.args.get('password')
+        if(username=='mky' and password=='123'):
+            return render_template('hello.html', name=username)
+        else:
+            error='Invalid username and password GET'
+    return render_template('login.html', error=error)
 
 # url_for(func_name, args)
 @app.route('/try_url/')
